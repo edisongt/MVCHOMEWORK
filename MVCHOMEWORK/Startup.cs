898b0cbce8894163ad048ebc211467db34dd6c31
Edison <Edison@EDISON-PC>
2016-10-16 21:49:02 +0800

    加入專案檔案。

---
+++ MVCHOMEWORK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHOMEWORK.Startup))]
+namespace MVCHOMEWORK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
